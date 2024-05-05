892dcf7ca7c11b798641537176c84a1792f0dc21
hoanghai9650 <hoanghai9650@gmail.com>
2024-05-05 11:35:04 +0700

    minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,138 +1,140 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {AppState, NativeModules, Platform, requireNativeComponent, TextInput, TextInputProps,} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  AppState,
+  Platform,
+  requireNativeComponent,
+  TextInput,
+} from 'react-native';
+import type { TextInputProps } from 'react-native';
 
-const {State: TextInputState} = TextInput;
-const {CalculatorKeyboardModule} = NativeModules;
+const { State: TextInputState } = TextInput;
 
 interface InputCalculatorProps extends TextInputProps {
-    forwardedRef?: React.Ref<any>;
+  text?: string;
+  forwardedRef?: React.Ref<any>;
 }
 
 export const Keys = {
-    CALCULATOR_TEXT_INPUT_FOCUSED: 'CALCULATOR_TEXT_INPUT_FOCUSED',
-    CALCULATOR_TEXT_INPUT_UNFOCUSED: 'CALCULATOR_TEXT_INPUT_UNFOCUSED',
-    CALCULATOR_DISMISS: 'CALCULATOR_DISMISS',
-    CALCULATOR_CALCULATE: 'CALCULATOR_CALCULATE',
-    CALCULATOR_HEIGHT_UPDATED: 'CALCULATOR_HEIGHT_UPDATED',
-    CALULATOR_PUSH_KEY: 'CALULATOR_PUSH_KEY',
+  CALCULATOR_TEXT_INPUT_FOCUSED: 'CALCULATOR_TEXT_INPUT_FOCUSED',
+  CALCULATOR_TEXT_INPUT_UNFOCUSED: 'CALCULATOR_TEXT_INPUT_UNFOCUSED',
+  CALCULATOR_DISMISS: 'CALCULATOR_DISMISS',
+  CALCULATOR_CALCULATE: 'CALCULATOR_CALCULATE',
+  CALCULATOR_HEIGHT_UPDATED: 'CALCULATOR_HEIGHT_UPDATED',
+  CALULATOR_PUSH_KEY: 'CALULATOR_PUSH_KEY',
 };
 
 const InputCalculator: React.FC<InputCalculatorProps> = (props) => {
-    const [isFocus, setIsFocus] = useState(false);
-    const [lastNativeText, setLastNativeText] = useState(props.value);
-    const inputRef = useRef(null);
+  const [isFocus, setIsFocus] = useState(false);
+  const [lastNativeText, setLastNativeText] = useState(props.value);
+  const inputRef = useRef<TextInput>(null);
 
-    useEffect(() => {
-        const nativeProps = {};
-        if (lastNativeText !== props.value && typeof props.value === 'string') {
-            // @ts-ignore
-            nativeProps.text = props.value;
-        }
-        if (
-            Object.keys(nativeProps).length > 0 &&
-            inputRef.current &&
-            inputRef.current.setNativeProps
-        ) {
-            inputRef.current.setNativeProps(nativeProps);
-        }
+  useEffect(() => {
+    const nativeProps = {};
+    if (lastNativeText !== props.value && typeof props.value === 'string') {
+      // @ts-ignore
+      nativeProps.text = props.value;
+    }
+    if (
+      Object.keys(nativeProps).length > 0 &&
+      inputRef.current &&
+      inputRef.current.setNativeProps
+    ) {
+      inputRef.current.setNativeProps(nativeProps);
+    }
 
-        if (props.selectionState && props.selection) {
-            props.selectionState.update(props.selection.start, props.selection.end);
-        }
-    }, [props.value, props.selection, props.selectionState, lastNativeText]);
+    if (props.selectionState && props.selection && props.selection.end) {
+      props.selectionState.update(props.selection.start, props.selection.end);
+    }
+  }, [props.value, props.selection, props.selectionState, lastNativeText]);
 
-    useEffect(() => {
-        AppState.addEventListener('change', handleAppStateChange);
-        return () => {
-            AppState.removeEventListener('change', handleAppStateChange);
-        };
-    }, []);
-
-    const handleAppStateChange = (nextAppState: string) => {
-        if (nextAppState === 'background' && Platform.OS === 'android') {
-            blur();
-        }
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.addEventListener('change', handleAppStateChange);
     };
+  }, []);
 
-    const blur = () => {
-        if (inputRef.current && typeof inputRef.current.blur === 'function') {
-            inputRef.current.blur();
-        }
-    };
+  const handleAppStateChange = (nextAppState: string) => {
+    if (nextAppState === 'background' && Platform.OS === 'android') {
+      blur();
+    }
+  };
 
-    const focus = () => {
-        if (inputRef.current && typeof inputRef.current.focus === 'function') {
-            inputRef.current.focus();
-        }
-    };
+  const blur = () => {
+    if (inputRef.current && typeof inputRef.current.blur === 'function') {
+      inputRef.current.blur();
+    }
+  };
 
-    const getText = () => {
-        if (typeof props.value === 'string') {
-            console.log('props.value', props.value);
-            return props.value;
-        }
-        if (typeof props.defaultValue === 'string') {
-            return props.defaultValue;
-        }
+  const focus = () => {
+    if (inputRef.current && typeof inputRef.current.focus === 'function') {
+      inputRef.current.focus();
+    }
+  };
 
-        return '';
-    };
+  const getText = () => {
+    if (typeof props.value === 'string') {
+      console.log('props.value', props.value);
+      return props.value;
+    }
+    if (typeof props.defaultValue === 'string') {
+      return props.defaultValue;
+    }
 
-    const onFocus = (event) => {
-        setIsFocus(true);
-        if (props.onFocus) {
-            props.onFocus(event);
-        }
-        if (inputRef.current != null && TextInputState) {
-            TextInputState.focusTextInput(inputRef.current);
-        }
-    };
+    return '';
+  };
 
-    const onBlur = (event) => {
-        setIsFocus(false);
-        if (props.onBlur) {
-            props.onBlur(event);
-        }
-    };
+  const onFocus = (event: any) => {
+    setIsFocus(true);
+    if (props.onFocus) {
+      props.onFocus(event);
+    }
+    if (inputRef.current != null && TextInputState) {
+      TextInputState.focusTextInput(inputRef.current);
+    }
+  };
 
-    const onChange = (event) => {
-        if (inputRef.current && inputRef.current.setNativeProps) {
-            inputRef.current.setNativeProps({
-                mostRecentEventCount: event.nativeEvent.eventCount,
-            });
-        }
+  const onBlur = (event: any) => {
+    setIsFocus(false);
+    if (props.onBlur) {
+      props.onBlur(event);
+    }
+  };
 
-        const {text} = event.nativeEvent;
-        if (props.onChange) {
-            props.onChange(event);
-        }
-        if (props.onChangeText) {
-            props.onChangeText(text);
-        }
-        if (!inputRef) {
-            return;
-        }
-        setLastNativeText(text);
-    };
+  const onChange = (event: any) => {
+    if (inputRef.current && inputRef.current.setNativeProps) {
+      inputRef.current.setNativeProps({
+        mostRecentEventCount: event.nativeEvent.eventCount,
+      });
+    }
 
+    const { text } = event.nativeEvent;
+    if (props.onChange) {
+      props.onChange(event);
+    }
+    if (props.onChangeText) {
+      props.onChangeText(text);
+    }
+    if (!inputRef) {
+      return;
+    }
+    setLastNativeText(text);
+  };
 
-    return (
-        <RCTInputCalculator
-            {...props}
-            ref={inputRef}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            onChange={onChange}
-            text={getText()}
-        />
-    );
+  return (
+    <RCTInputCalculator
+      {...props}
+      ref={inputRef}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onChange={onChange}
+      text={getText()}
+    />
+  );
 };
 
 // @ts-ignore
-const RCTInputCalculator = requireNativeComponent(
-    'RCTInputCalculator',
-    InputCalculator,
-    {}
-);
+const RCTInputCalculator =
+  requireNativeComponent<InputCalculatorProps>('RCTInputCalculator');
 
 export default InputCalculator;
